a4eb94cc565f488a2591c1e2ff5bf68b1d7998f6
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-11 10:35:45 +0200

    ćwiczenia w konosli_JS_DOM

---
+++ DOM/znajdywanie_elementow.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+var parFirts = document.getElementById('parFirst');
+console.log(parFirts);
+
+var linki = document.getElementsByClassName('superlink');
+console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+var divPoID= document.querySelector('#parSecond');
+console.log(divPoID);
+
+var pierwszyLinkiPoSelektorze =
+document.querySelector('.superlink');
+console.log(pierwszyLinkiPoSelektorze);
+
+var linkiPoSelektorze =
+document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
+
+
+
